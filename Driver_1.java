33e2e61bd5367d96fa1753ab6b320bb1b799fed1
Jahanzaib Manzoor Palh <61579346+Jahanzaib523@users.noreply.github.com>
2022-08-26 18:26:41 +0500

    Add files via upload

---
+++ Driver_1.java
@@ -0,0 +1,15 @@
+package exercise;
+
+public class Driver_1 {
+
+	public Driver_1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World! File 1");
+
+	}
+
+}
